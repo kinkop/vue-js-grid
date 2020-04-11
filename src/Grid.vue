b014d69eb0db8543e9b25f276a850602581db202
Pakin Mankong <pakinmankong@gmail.com>
2020-04-11 13:51:09 +0700

    Debug

--- src/Grid.vue
+++ src/Grid.vue
@@ -28,6 +28,31 @@
 import windowSize from './mixins/window_size.js'
 import GridItem from './GridItem.vue'
 
+const compareValues = (key, order = 'asc') => {
+  return function innerSort(a, b) {
+    if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+      // property doesn't exist on either object
+      return 0
+    }
+
+    const varA = typeof a[key] === 'string' ? a[key].toUpperCase() : a[key]
+    const varB = typeof b[key] === 'string' ? b[key].toUpperCase() : b[key]
+
+    let comparison = 0
+    if (varA > varB) {
+      comparison = 1
+    } else if (varA < varB) {
+      comparison = -1
+    }
+    return order === 'desc' ? comparison * -1 : comparison
+  }
+}
+
+export const sort = (array, sortColumn, order = 'asc') => {
+  return array.sort(compareValues(sortColumn, order))
+}
+
+
 export default {
   name: 'Grid',
   mixins: [windowSize],
@@ -231,6 +256,8 @@ export default {
 
           return item
         })
+
+        this.list.sort(compareValues('sort'))
 
         this.$emit('sort', this.wrapEvent())
       }
